e7552e4674c4d17879a30c63cf532951d34f9359
SeanSyyue <Sean962081468@gmail.com>
2018-06-02 09:45:48 +0800

    Capitalize constants; simplify currency list

--- TableManager.py
+++ TableManager.py
@@ -1,6 +1,8 @@
 import re
 import os
+import time
 import requests
+import numpy as np
 
 URL = 'http://rate.bot.com.tw/xrt/flcsv/0/day?Lang=en-US'
 DOWNLOAD_FOLDER = 'exchange-rate-tables'
@@ -24,6 +26,15 @@ def download_csv(folder=DOWNLOAD_FOLDER):
                 print("Download completed")
         else:
             print("Post failed with status code: {}".format(post.status_code))
+
+
+def search_newest_file(folder):
+    def get_timestamp(target):
+        time_str = re.search(r'@(.*?)\.', target).group(1)
+        return time.strptime(time_str, '%Y%m%d%H%M')
+
+    file_list = os.listdir(folder)
+    return file_list[int(np.argmax(map(get_timestamp, file_list)))]
 
 
 if __name__ == '__main__':
