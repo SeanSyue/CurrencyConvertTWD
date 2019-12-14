19492a51fa01473fcbda48d52acf2f6648aa15f7
SeanSyue <sean962081468@gmail.com>
2019-12-14 12:20:13 +0800

    expose FileSystemManager and OnlineResourceManager

--- currency_converter_twd/TableManager.py
+++ currency_converter_twd/TableManager.py
@@ -16,128 +16,131 @@ _URL = config['TABLE']['url']
 _FOLDER = config['TABLE']['folder']
 
 
-class TableManager:
-    class __FileSystemManager:
-        __active_table: str
-        __outdated_tables: List[str]
+class FileSystemManager:
+    __active_table: str
+    __outdated_tables: List[str]
 
-        def __init__(self, root_):
-            """
-            Store names of different currency tables in the file system
-            @var self.__active_table: names of active currency table
-            @var self.__outdated_tables: names of outdated currency table
-            @param root_: str or PathLike object. root directory where table folder exists
-            """
-            self.__active_table = ''
+    def __init__(self, root_):
+        """
+        Store names of different currency tables in the file system
+        @var self.__active_table: names of active currency table
+        @var self.__outdated_tables: names of outdated currency table
+        @param root_: str or PathLike object. root directory where table folder exists
+        """
+        self.__active_table = ''
+        self.__outdated_tables = list()
+        self.__table_folder = Path(root_).joinpath(_FOLDER)
+
+    @property
+    def outdated_tables(self):
+        return self.__outdated_tables
+
+    @property
+    def active_table(self):
+        return self.__active_table
+
+    @outdated_tables.setter
+    def outdated_tables(self, tables):
+        """
+        initialize self.__outdated_tables with list of table names
+        @param tables: names of outdated currency table
+        """
+        if not isinstance(tables, list):
+            raise TypeError("Variable `outdated_tables` should be initialized with an list object!")
+
+        # empty `self.__outdated_tables` before re-populating
+        if len(self.__outdated_tables) is not 0:
             self.__outdated_tables = list()
-            self.__table_folder = Path(root_).joinpath(_FOLDER)
+        self.__outdated_tables = tables
 
-        @property
-        def outdated_tables(self):
-            return self.__outdated_tables
+    @active_table.setter
+    def active_table(self, table):
+        """
+        set new active currency table, and move the previous one into the `__outdated_tables` list
+        @param table: names of new currency table
+        """
+        if self.__active_table is not '':
+            self.__outdated_tables.append(self.__active_table)
+        self.__active_table = table
 
-        @property
-        def active_table(self):
-            return self.__active_table
+    def initialize_table_folder(self):
+        """
+        initialize currency table under certain directory
+        """
+        # TODO: if `self.__table_folder` is not existed, then mkdir
+        pass
 
-        @outdated_tables.setter
-        def outdated_tables(self, tables):
-            """
-            initialize self.__outdated_tables with list of table names
-            @param tables: names of outdated currency table
-            """
-            if not isinstance(tables, list):
-                raise TypeError("Variable `outdated_tables` should be initialized with an list object!")
+    def translate_to_abs_table_path(self, name_):
+        """
+        translate to the absolute pathname of a currency table
+        by referencing the file system
+        @param name_: the name of a currency table
+        @return: PathLike object
+        """
+        return self.__table_folder.joinpath(name_)
 
-            # empty `self.__outdated_tables` before re-populating
-            if len(self.__outdated_tables) is not 0:
-                self.__outdated_tables = list()
-            self.__outdated_tables = tables
+    @staticmethod
+    def get_relevant_table_name(path_):
+        """
+        get the name of a currency table by referencing the file system
+        @param path_: the absolute filepath of a currency table
+        @return: str object
+        """
+        return Path(path_).name
 
-        @active_table.setter
-        def active_table(self, table):
-            """
-            set new active currency table, and move the previous one into the `__outdated_tables` list
-            @param table: names of new currency table
-            """
-            if self.__active_table is not '':
-                self.__outdated_tables.append(self.__active_table)
-            self.__active_table = table
+    def delete_outdated_tables(self):
+        # TODO: For tables in self.outdated_tables, do delete
+        # TODO: self.outdated_tables = list()
+        # TODO: Catch `FileNotFoundError`
+        pass
 
-        def initialize_table_folder(self):
-            """
-            initialize currency table under certain directory
-            """
-            # TODO: if `self.__table_folder` is not existed, then mkdir
-            pass
 
-        def translate_to_abs_table_path(self, name_):
-            """
-            translate to the absolute pathname of a currency table
-            by referencing the file system
-            @param name_: the name of a currency table
-            @return: PathLike object
-            """
-            return self.__table_folder.joinpath(name_)
-        
-        @staticmethod
-        def get_relevant_table_name(path_):
-            """
-            get the name of a currency table by referencing the file system
-            @param path_: the absolute filepath of a currency table
-            @return: str object
-            """
-            return Path(path_).name
-
-        def delete_outdated_tables(self):
-            # TODO: For tables in self.outdated_tables, do delete
-            # TODO: self.outdated_tables = list()
-            # TODO: Catch `FileNotFoundError`
-            pass
-
-    class __OnlineResourceManager:
-        __url = _URL
-
-        def __init__(self):
-            self.__resource = None
-            self.__resource_table_name = None
-
-        def __query_table_resource(self):
-            """
-            post online currency table url, used for checking connectivity
-            @return status code
-            """
-            # TODO: with requests.Session(), post `__url`, if network connections failed, return status code
-            # TODO: if code==200, self.__resource = resource
-            pass
-
-        def __parse_resource_table_name(self):
-            """
-            @return return the name of the queried currency table for the resource
-            """
-            # TODO: _get_resource_table_name using `re` module
-            # TODO: self.__resource_table_name = resource_table_name
-            pass
-        
-        def fetch_latest_table_name(self):
-            """
-            wrapper function. 
-            """
-            self.__query_table_resource()
-            # TODO: if status == 200, `self.__parse_resource_table_name()`
-            pass
-
-        def download_table(self, destination_):
-            """
-            download a new table from the online resource
-            @param destination_: the destination pathname of the newly downloaded table
-            """
-            # TODO: download currency table to target
-            pass
+class OnlineResourceManager:
+    __url = _URL
 
     def __init__(self):
-        self.__sys_mgr = self.__FileSystemManager(Path(__file__).parent)
-        self.__res_mgr = self.__OnlineResourceManager()
+        self.__resource = None
+        self.__resource_table_name = None
+
+    def __query_table_resource(self):
+        """
+        post online currency table url, used for checking connectivity
+        @return status code
+        """
+        # TODO: with requests.Session(), post `__url`, if network connections failed, return status code
+        # TODO: if code==200, self.__resource = resource
+        pass
+
+    def __parse_resource_table_name(self):
+        """
+        @return return the name of the queried currency table for the resource
+        """
+        # TODO: _get_resource_table_name using `re` module
+        # TODO: self.__resource_table_name = resource_table_name
+        pass
+
+    def fetch_latest_table_name(self):
+        """
+        wrapper function.
+        """
+        self.__query_table_resource()
+        # TODO: if status == 200, `self.__parse_resource_table_name()`
+        pass
+
+    def download_table(self, destination_):
+        """
+        download a new table from the online resource
+        @param destination_: the destination pathname of the newly downloaded table
+        """
+        # TODO: download currency table to target
+        pass
+
+
+class TableManager:
+
+    def __init__(self):
+        self.__sys_mgr = FileSystemManager(Path(__file__).parent)
+        self.__res_mgr = OnlineResourceManager()
 
     def initialize(self):
         # TODO: Check if table folder exists
@@ -152,7 +155,7 @@ class TableManager:
         # TODO: download
         # TODO: renew self.active_table
         pass
-    
+
     def clean_up(self):
         if len(self.__sys_mgr.outdated_tables) is 0:
             print("No outdated tables detected")
